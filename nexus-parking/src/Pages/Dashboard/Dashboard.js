a32eeb2817f0c3081b5f1a2eff20abda53241e51
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-03-20 23:55:06 -0300

    Menu dashboard

--- nexus-parking/src/Pages/Dashboard/Dashboard.js
+++ nexus-parking/src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,65 @@
 import React from 'react';
 
+// Extern Components
 import { Container } from '../../Components/Container/Container';
+
+import { IoIosSearch } from 'react-icons/io'
+
+// Intern Styles
+import {
+  InternContainer,
+  Title,
+  SticksEffect,
+  DarkSticky,
+  OrangeSticky,
+  Menu,
+  MenuTitleDiv,
+  MenuTitle,
+  MenuDescription,
+  SearchCamp,
+  SearchButton
+} from './styles'
 
 export default function Dashboard() {
   return (
     <Container>
-      <h1>AOJISOFJIDJIASODJIFJIOUASD</h1>
+      <InternContainer>
+        <Title>Início</Title>
+        <SticksEffect>
+          <div>
+            <DarkSticky/>
+          </div>
+          <div>
+            <OrangeSticky/>
+          </div>
+        </SticksEffect>
+
+        <Menu>
+          <MenuTitleDiv>
+              <MenuTitle>
+                Carros Estacionados
+              </MenuTitle>
+              <MenuDescription>
+                Exibindo todos os carros estacionados no momento
+              </MenuDescription>
+          </MenuTitleDiv>
+
+          <SearchCamp>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">
+                  <IoIosSearch/>
+                </span>
+              </div>
+              <input type="text" placeholder='Digite a placa para buscar o carro...' class="form-control"/>
+            </div>
+
+            <SearchButton>
+              Buscar
+            </SearchButton>
+          </SearchCamp>
+        </Menu>
+      </InternContainer>
     </Container>
   );
 }
---
+++ nexus-parking/src/Pages/Dashboard/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+export const InternContainer = styled.div`
+    margin-left: 30px;
+    margin-top: 50px;
+`
+
+export const Title = styled.h1`
+    font-size: 50px;
+    color: #FFA43B;
+    font-weight: bold;
+    margin-bottom: 0;
+`
+
+export const SticksEffect = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding-bottom: 30px;
+`
+
+export const DarkSticky = styled.hr`
+    width: 40px;
+    height: 16px;
+    background-color: #424242;
+    border-radius: 15px;
+    margin-right: 15px;
+`
+
+export const OrangeSticky = styled.hr`
+    width: 150px;
+    height: 16px;
+    background-color: #FFA43B;
+    border-radius: 15px;
+`
+
+export const Menu = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+`
+
+export const MenuTitleDiv = styled.div`
+    
+`
+
+export const MenuTitle = styled.h1`
+    font-size: 24px;
+    color: #FFA43B;
+    font-weight: bold;
+`
+
+export const MenuDescription = styled.p`
+    color: #424242;
+    font-weight: bold;
+`
+
+export const SearchCamp = styled.div`
+    margin-right: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    margin-bottom: 24px;
+
+
+    span{
+        height: 37px;
+    }
+
+    input{
+        width: 450px;
+        height: 37px;
+        margin-right: 20px;
+    }
+`
+
+export const SearchButton = styled.button`
+    width: 150px;
+    height: 37px;
+    background-color: #FFA43B;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    font-weight: bold;
+`
+
+
