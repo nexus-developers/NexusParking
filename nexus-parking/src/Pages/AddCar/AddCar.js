489d82629503dc1c6c9ce86a3fddf6fccd0c8ae7
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-06-11 13:58:09 -0300

    Cadastro de veiculos

--- nexus-parking/src/Pages/AddCar/AddCar.js
+++ nexus-parking/src/Pages/AddCar/AddCar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux'
 
+import Api from '../../Services/Api';
+
 // Extern Components
 import { Container, InternContainer } from '../../Components/Container/Container';
 
@@ -21,6 +23,59 @@ import {
 import { MdClose } from 'react-icons/md'
 
 class AddCar extends Component {
+
+    state = {
+        car: false,
+        moto: false
+    }
+
+    handleVehicleRegister = async () => {
+        const { car, moto } = this.state;
+        const color = this.refs.color.value;
+        const model = this.refs.model.value;
+        const plate = this.refs.plate.value;
+        const owner_name = this.refs.owner_name.value;
+        const owner_cpf = this.refs.owner_cpf.value;
+        const owner_ddd = '081';
+        const owner_phone = this.refs.owner_phone.value;
+        const owner_email = this.refs.owner_email.value;
+        let info = {}
+
+        if(car){
+            info = { 
+                car, 
+                color, 
+                model, 
+                plate, 
+                owner_name, 
+                owner_cpf,
+                owner_ddd,
+                owner_phone,
+                owner_email
+            }
+        }
+        else{
+            info = { 
+                moto, 
+                color, 
+                model, 
+                plate, 
+                owner_name, 
+                owner_cpf,
+                owner_ddd,
+                owner_phone,
+                owner_email
+            }
+        }
+
+        try {
+            await Api.post('/parking', info);
+
+            this.closeModal();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     closeModal = () => {
         const { dispatch } = this.props;
@@ -53,21 +108,33 @@ class AddCar extends Component {
                                                 <Form1>
                                                     <div>
                                                         <h5>Nome do Proprietário:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='owner_name'
+                                                        />
                                                     </div>
                                                     <div>
                                                         <h5>CPF:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='owner_cpf'
+                                                        />
                                                     </div>
                                                     <div>
                                                         <h5>E-mail:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='owner_email'
+                                                        />
                                                     </div>
                                                 </Form1>
                                                 <Form2>
                                                     <div>
                                                         <h5>Telefone:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='owner_phone'
+                                                        />
                                                     </div>
                                                 </Form2>
 
@@ -76,28 +143,50 @@ class AddCar extends Component {
                                                 <Form1>
                                                     <div>
                                                         <h5>Modelo:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='model'
+                                                        />
                                                     </div>
                                                     <div>
                                                         <h5>Placa:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='plate'
+                                                        />
                                                     </div>
                                                     <div>
                                                         <h5>Cor:</h5>
-                                                        <input className='form-control'/>
+                                                        <input 
+                                                            className='form-control'
+                                                            ref='color'
+                                                        />
                                                     </div>
                                                 </Form1>
                                                 <Form2>
                                                     <FormCheck>
                                                     <h5>Tipo:</h5>
                                                         <div class="form-check">
-                                                            <input class="form-check-input" type="radio" name="Radios" id="Radios1" value="option1"/>
+                                                            <input 
+                                                                class="form-check-input" 
+                                                                type="radio" name="Radios" 
+                                                                id="Radios1" 
+                                                                value="option1"
+                                                                onChange={() => this.setState({car: true, moto: false})}
+                                                            />
                                                             <label class="form-check-label" for="Radios1">
                                                                 Carro
                                                             </label>
                                                         </div>
                                                         <div class="form-check">
-                                                            <input class="form-check-input" type="radio" name="Radios" id="Radios2" value="option2"/>
+                                                            <input 
+                                                                class="form-check-input"
+                                                                type="radio" 
+                                                                name="Radios" 
+                                                                id="Radios2" 
+                                                                value="option2"
+                                                                onChange={() => this.setState({moto: true, car: false})}
+                                                            />
                                                             <label class="form-check-label" for="Radios2">
                                                                 Moto
                                                             </label>
@@ -107,7 +196,9 @@ class AddCar extends Component {
                                             </FormAddClient>
 
                                             <CreateClientButton>
-                                                <button>
+                                                <button
+                                                    onClick={() => this.handleVehicleRegister()}
+                                                >
                                                     Cadastrar
                                                 </button>
                                             </CreateClientButton>
